c7e3a65c76b7cedfe5838cfa7feb376f9925b1d6
LeoJasmim <leojasmim@hotmail.com>
2017-08-28 13:59:12 -0300

    Section07_Lesson35 - Clock implementation

--- app/src/main/java/com/ljasmim/relogiodecabeceira/MainActivity.java
+++ app/src/main/java/com/ljasmim/relogiodecabeceira/MainActivity.java
@@ -1,13 +1,20 @@
 package com.ljasmim.relogiodecabeceira;
 
+import android.os.Handler;
+import android.os.SystemClock;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.CheckBox;
 import android.widget.TextView;
 
+import java.util.Calendar;
+
 public class MainActivity extends AppCompatActivity {
 
     private ViewHolder mViewHolder;
+    private Handler mHandler; //Manipulador do Runnable
+    private Runnable mRunnable;
+    private boolean mIsRunnableStopped;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,13 +23,65 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         this.mViewHolder = new ViewHolder();
+        this.mHandler = new Handler();
 
         this.mViewHolder.mTextHourMinute = (TextView) findViewById(R.id.text_hour_minute);
         this.mViewHolder.mTextSeconds = (TextView) findViewById(R.id.text_seconds);
         this.mViewHolder.mCheckBattery = (CheckBox) findViewById(R.id.check_battery);
+
     }
 
-    private static class ViewHolder{
+    @Override
+    protected void onResume() {
+        super.onResume();
+        mIsRunnableStopped = false;
+        this.startBedsideClock();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        mIsRunnableStopped = true;
+    }
+
+    private void startBedsideClock() {
+
+        final Calendar calendar = Calendar.getInstance();
+
+        this.mRunnable = new Runnable() {
+            @Override
+            public void run() {
+
+                //Se a thread estiver parada não realiza a execução
+                if(mIsRunnableStopped)
+                    return;
+
+                //Captura a hora do sistema
+                calendar.setTimeInMillis(System.currentTimeMillis());
+
+                //Obtém as strings do horário
+                String hourMinutesFormat = String.format("%02d:%02d", calendar.get(Calendar.HOUR_OF_DAY),
+                        calendar.get(Calendar.MINUTE));
+                String secondsFormat = String.format("%02d", calendar.get(Calendar.SECOND));
+
+                //Ajusta os valores dos itens de layout
+                mViewHolder.mTextHourMinute.setText(hourMinutesFormat);
+                mViewHolder.mTextSeconds.setText(secondsFormat);
+
+                //Calcula quando será a próxima execução
+                long now = SystemClock.uptimeMillis();
+                long next = now + (1000 - (now % 1000));
+
+                //Realiza a próxima execução
+                mHandler.postAtTime(mRunnable, next);
+            }
+        };
+
+        this.mRunnable.run();
+
+    }
+
+    private static class ViewHolder {
         TextView mTextHourMinute;
         TextView mTextSeconds;
         CheckBox mCheckBattery;
