8e03be7f44dc2e03a77a1e6c4b709ce94c0c1919
LeoJasmim <leojasmim@hotmail.com>
2017-08-28 10:43:54 -0300

    Section07_Lesson34 - Initialize layout

--- app/src/main/java/com/ljasmim/relogiodecabeceira/MainActivity.java
+++ app/src/main/java/com/ljasmim/relogiodecabeceira/MainActivity.java
@@ -2,13 +2,30 @@ package com.ljasmim.relogiodecabeceira;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.CheckBox;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ViewHolder mViewHolder;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_main);
+
+        this.mViewHolder = new ViewHolder();
+
+        this.mViewHolder.mTextHourMinute = (TextView) findViewById(R.id.text_hour_minute);
+        this.mViewHolder.mTextSeconds = (TextView) findViewById(R.id.text_seconds);
+        this.mViewHolder.mCheckBattery = (CheckBox) findViewById(R.id.check_battery);
+    }
+
+    private static class ViewHolder{
+        TextView mTextHourMinute;
+        TextView mTextSeconds;
+        CheckBox mCheckBattery;
     }
 
 }
